b860cf9381788887d336e231ff6dc57651d37442
Ricevind <ricevind@gmail.com>
2018-09-16 11:49:11 +0200

    init service test

---
+++ __tests__/test.products-service.js
@@ -0,0 +1,7 @@
+import productService from '../front-end/scripts/services/products-service';
+
+describe('ProductsService', () => {
+    it('exists', () => {
+        expect(productService).toBeDefined();
+    });
+});
