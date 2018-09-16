191a59b7a8d3d939d7409000066686a8da462e71
Ricevind <ricevind@gmail.com>
2018-09-16 12:41:23 +0200

    products service tests

--- __tests__/test.products-service.js
+++ __tests__/test.products-service.js
@@ -4,4 +4,33 @@ describe('ProductsService', () => {
     it('exists', () => {
         expect(productService).toBeDefined();
     });
+
+    describe('Method: fetchProducts', () => {
+        it('is function', () => {
+            expect(typeof productService.fetchProducts).toBe('function');
+        });
+
+        describe('When: called', () => {
+            let returnedValue = null;
+
+            beforeEach(async () => {
+                global.fetch = jest.fn().mockReturnValue(Promise.resolve());
+                returnedValue = productService.fetchProducts();
+                console.log(returnedValue)
+            });
+
+            it('calls fetch', () => {
+                expect(fetch).toHaveBeenCalled();
+            });
+
+            it('calls fetch with url', () => {
+                const expectedUrl = 'http://localhost:3000/products';
+                expect(fetch).toHaveBeenCalledWith(expectedUrl);
+            });
+
+            it('returns Promise', () => {
+                expect(typeof returnedValue.then).toBe('function');
+            });
+        });
+    });
 });
--- front-end/scripts/services/products-service.js
+++ front-end/scripts/services/products-service.js
@@ -1,3 +1,10 @@
-class ProductService {}
+class ProductService {
+    api = 'http://localhost:3000'
+
+    fetchProducts() {
+        const url = `${this.api}/products`;
+        return fetch(url);
+    }
+}
 
 export default new ProductService();
